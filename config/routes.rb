ed143440928bda3b553f3574778719f82e8cbffe
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-18 22:09:05 +0900

    User モデルの、ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "users#index"
   devise_for :users, controllers: {
                        registrations: "users/registrations",
+                       sessions: "users/sessions",
                      }
   resources :users, only: [:index, :show]
 end
