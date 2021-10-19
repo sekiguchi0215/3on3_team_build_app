6ada6e34b854236eb032efc1495b0720b1381042
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-19 21:41:58 +0900

    デッキ登録機能を実装

--- app/controllers/deck_lists_controller.rb
+++ app/controllers/deck_lists_controller.rb
@@ -6,9 +6,12 @@ class DeckListsController < ApplicationController
   end
 
   def new
+    @deck = DeckList.new
   end
 
   def create
+    deck_list = current_user.deck_lists.create!(deck_list_params)
+    redirect_to user_path(current_user.id)
   end
 
   def edit
@@ -18,5 +21,11 @@ class DeckListsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def deck_list_params
+    params.require(:deck_list).permit(:deck_list, :status, :content)
   end
 end
