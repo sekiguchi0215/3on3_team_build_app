9853c6253a99982017834cb2ce099f93f4aa1971
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-21 23:25:04 +0900

    他のユーザーがデッキを編集・削除できないようにコントローラの処理を修正

--- app/controllers/deck_lists_controller.rb
+++ app/controllers/deck_lists_controller.rb
@@ -1,4 +1,6 @@
 class DeckListsController < ApplicationController
+  before_action :set_deck, only: %i[edit update destroy]
+
   def index
     @decks = current_user.deck_lists.order(:updated_at)
   end
@@ -17,18 +19,15 @@ class DeckListsController < ApplicationController
   end
 
   def edit
-    @deck = DeckList.find(params[:id])
   end
 
   def update
-    @deck = DeckList.find(params[:id])
     @deck.update(deck_list_params)
-    redirect_to @deck
+    redirect_to user_path(current_user.id)
   end
 
   def destroy
-    deck = DeckList.find(params[:id])
-    deck.destroy
+    @deck.destroy
     redirect_to deck_lists_path
   end
 
@@ -36,5 +35,10 @@ class DeckListsController < ApplicationController
 
   def deck_list_params
     params.require(:deck_list).permit(:deck_list, :status, :content)
+  end
+
+  def set_deck
+    @deck = current_user.deck_lists.find_by(id: params[:id])
+    redirect_to root_path, alert: "権限がありません。" if @deck.nil?
   end
 end
