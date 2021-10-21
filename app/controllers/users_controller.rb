37b1a73b8d1b04dc9900458ce16c49142b4684e4
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-21 20:38:25 +0900

    ユーザー詳細ページに、そのユーザーが公開しているデッキを表示するように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if @user.id == current_user.id
+      @decks = @user.deck_lists.includes(:user).order(:updated_at)
+    else
+      @decks = @user.deck_lists.includes(:user).where(status: "public")
+    end
   end
 end
