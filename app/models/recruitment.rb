0d158e6afdfe64f278ba978cdf6bd7206eac05a3
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-22 21:32:59 +0900

    Recruitment モデルのバリデーションを記述

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -1,3 +1,7 @@
 class Recruitment < ApplicationRecord
   belongs_to :user
+
+  validates :event_title, presence: true
+  validates :number_of_applicants, presence: true
+  validates :recruitment_condition, presence: true
 end
