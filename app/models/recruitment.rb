e022d59456dc10fff463f4412e344f5315e33f1c
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-22 21:38:22 +0900

    対象のカラムを列挙型に設定

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -4,4 +4,7 @@ class Recruitment < ApplicationRecord
   validates :event_title, presence: true
   validates :number_of_applicants, presence: true
   validates :recruitment_condition, presence: true
+
+  enum number_of_applicants: [:one, :two], _prefix: true
+  enum recruitment_condition: [:anyone, :enjoy, :seriousness], _prefix: true
 end
