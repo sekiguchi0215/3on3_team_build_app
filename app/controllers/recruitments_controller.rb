18562ebd4a3ab3211bdb511843fcbdd822cd7493
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-23 21:29:40 +0900

    各機能の処理を修正

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,11 +1,18 @@
 class RecruitmentsController < ApplicationController
+  before_action :set_recruitment, only: %i[edit update destroy]
+
   def new
     @recruitment = Recruitment.new
   end
 
   def create
-    recruitment = current_user.recruitments.create!(recruitment_params)
-    redirect_to recruitment
+    @recruitment = current_user.recruitments.new(recruitment_params)
+    if @recruitment.save
+      redirect_to recruitments_path, notice: "メンバー募集を投稿しました。"
+    else
+      flash.now[:alert] = "投稿に失敗しました。"
+      render :new
+    end
   end
 
   def index
@@ -17,16 +24,18 @@ class RecruitmentsController < ApplicationController
   end
 
   def edit
-    @recruitment = Recruitment.find(params[:id])
   end
 
   def update
-    recruitment = Recruitment.update(recruitment_params)
-    redirect_to recruitment
+    if @recruitment.update(recruitment_params)
+      redirect_to @recruitment, notice: "更新しました。"
+    else
+      flash.now[:alert] = "更新に失敗しました。"
+      render :edit
+    end
   end
 
   def destroy
-    @recruitment = Recruitment.find(params[:id])
     @recruitment.destroy
     redirect_to recruitments_path, alert: "募集を削除しました。"
   end
@@ -35,5 +44,10 @@ class RecruitmentsController < ApplicationController
 
   def recruitment_params
     params.require(:recruitment).permit(:event_title, :number_of_applicants, :recruitment_condition, :introduction)
+  end
+
+  def set_recruitment
+    @recruitment = current_user.recruitments.find_by(id: params[:id])
+    redirect_to root_path, alert: "権限がありません。" if @recruitment.nil?
   end
 end
