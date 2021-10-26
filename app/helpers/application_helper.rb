4ec7cc28373d44b53830b3514af13a4a7f80153f
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-27 00:01:36 +0900

    横幅の最大値を設定するメソッドをヘルパーに記述

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def max_width
+    if controller_name == "recruitments"
+      "mw-md"
+    elsif devise_controller?
+      "mw-sm"
+    else
+      "mw-lg"
+    end
+  end
 end
