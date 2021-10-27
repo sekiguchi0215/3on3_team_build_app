360711ab5119cd7cde06a397111f54537b2bddcb
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-28 00:11:12 +0900

    Entry モデルにバリデーションを記述

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,9 @@
 class Entry < ApplicationRecord
   belongs_to :user
   belongs_to :recruitment
+
+  validates :user_id, uniqueness: {
+              scope: :recruitment_id,
+              message: "既に応募しています。",
+            }
 end
