6ad58e655654f0f753e2efdca24f1ac087f204b2
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-28 20:30:24 +0900

    自分の募集に応募できないように設定

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,7 +3,14 @@ class Entry < ApplicationRecord
   belongs_to :recruitment
 
   validates :user_id, uniqueness: {
-              scope: :recruitment_id,
-              message: "既に応募しています。",
-            }
+                        scope: :recruitment_id,
+                        message: "既に応募しています。",
+                      }
+  validate :my_recruitment_cannot_entry
+
+  private
+
+  def my_recruitment_cannot_entry
+    errors.add(:base, "自分の募集に応募はできません。") if user_id == recruitment.user_id
+  end
 end
