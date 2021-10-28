ccfa5660e510e0aac94659d969c2594d7d9a9e5c
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-29 00:28:02 +0900

    応募ボタンに非同期処理を実装

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,11 +1,11 @@
 class EntriesController < ApplicationController
   def create
-    current_user.entries.create!(recruitment_id: params[:recruitment_id])
-    redirect_back(fallback_location: root_path)
+    @recruitment = Recruitment.find(params[:recruitment_id])
+    current_user.entries.create!(recruitment_id: @recruitment.id)
   end
 
   def destroy
-    current_user.entries.find_by(recruitment_id: params[:recruitment_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @recruitment = Recruitment.find(params[:recruitment_id])
+    current_user.entries.find_by(recruitment_id: @recruitment.id).destroy!
   end
 end
