9424743a9a791da621a6b2e402831bffd58dec33
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-29 00:43:37 +0900

    応募数が表示されるよう、ビューに設定

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,11 +1,11 @@
 class EntriesController < ApplicationController
   def create
+    current_user.entries.create!(recruitment_id: params[:recruitment_id])
     @recruitment = Recruitment.find(params[:recruitment_id])
-    current_user.entries.create!(recruitment_id: @recruitment.id)
   end
 
   def destroy
+    current_user.entries.find_by(recruitment_id: params[:recruitment_id]).destroy!
     @recruitment = Recruitment.find(params[:recruitment_id])
-    current_user.entries.find_by(recruitment_id: @recruitment.id).destroy!
   end
 end
