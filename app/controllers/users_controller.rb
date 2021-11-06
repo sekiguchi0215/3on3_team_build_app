d123030624209f813d800b6eacae2d5ea5b9838b
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-06 23:58:03 +0900

    users コントローラにダイレクトメッセージに関連する処理を記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,20 @@ class UsersController < ApplicationController
       @decks = @user.deck_lists.includes(:user).where(status: "public")
     end
     @entries = @user.entried_recruitment
+    @currentUserRoomKey = RoomKey.where(user_id: current_user.id)
+    @userRoomKey = RoomKey.where(user_id: @user.id)
+
+    unless @user.id == current_user.id
+      @currentUserRoomKey.each do |current_user|
+        if current_user.room_id == user.room_id
+          @haveRoom = true
+          @roomId = current_user.room_id
+        end
+      end
+      unless @haveRoom
+        @room = Room.new
+        @room_key = RoomKey.new
+      end
+    end
   end
 end
