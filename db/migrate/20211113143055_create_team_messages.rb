c9656a957dda8302f6fd9eac3a28fcdf0e550fc4
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-13 23:34:40 +0900

    制約を追加して、マイグレーションを実行

--- db/migrate/20211113143055_create_team_messages.rb
+++ db/migrate/20211113143055_create_team_messages.rb
@@ -3,7 +3,7 @@ class CreateTeamMessages < ActiveRecord::Migration[6.1]
     create_table :team_messages do |t|
       t.references :user, null: false, foreign_key: true
       t.references :team, null: false, foreign_key: true
-      t.text :message
+      t.text :message, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_124258) do
+ActiveRecord::Schema.define(version: 2021_11_13_143055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,16 @@ ActiveRecord::Schema.define(version: 2021_11_13_124258) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "team_messages", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "team_id", null: false
+    t.text "message", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["team_id"], name: "index_team_messages_on_team_id"
+    t.index ["user_id"], name: "index_team_messages_on_user_id"
+  end
+
   create_table "teams", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -111,4 +121,6 @@ ActiveRecord::Schema.define(version: 2021_11_13_124258) do
   add_foreign_key "recruitments", "users"
   add_foreign_key "room_keys", "rooms"
   add_foreign_key "room_keys", "users"
+  add_foreign_key "team_messages", "teams"
+  add_foreign_key "team_messages", "users"
 end
