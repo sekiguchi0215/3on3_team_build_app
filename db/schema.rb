7a326250a6ee32f714dfce3ddc233a8a537eb41b
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-15 22:54:13 +0900

    TeamMessage モデルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_143055) do
+ActiveRecord::Schema.define(version: 2021_11_15_134552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2021_11_13_143055) do
     t.text "message", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_valid", default: true, null: false
     t.index ["team_id"], name: "index_team_messages_on_team_id"
     t.index ["user_id"], name: "index_team_messages_on_user_id"
   end
