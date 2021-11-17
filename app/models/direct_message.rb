47ea51eb49d322049483b41827ef6dd77da78b94
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-17 23:39:31 +0900

    ダイレクトメッセージを送ったときの通知を作るメソッドを記述

--- app/models/direct_message.rb
+++ app/models/direct_message.rb
@@ -4,4 +4,18 @@ class DirectMessage < ApplicationRecord
   has_many :notifications, dependent: :destroy
 
   validates :message, presence: true
+
+  def create_notificaton_direct_message!(current_user, visited_id)
+    temp = Notification.where(visitor_id: current_user.id, visited_id: visited_id, action: "direct_message")
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        visited_id: visited_id,
+        action: "direct_message",
+      )
+
+      notification.save if notification.valid?
+    else
+      temp.update(checked: false)
+    end
+  end
 end
