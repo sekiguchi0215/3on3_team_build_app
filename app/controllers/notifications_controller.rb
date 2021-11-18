614ba1aed81d099093531af23dc108c3b5a53d08
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-19 00:09:11 +0900

    通知機能の表示を修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,10 @@
 class NotificationsController < ApplicationController
   def index
-    @active_notifications = current_user.passive_notifications.where(checked: false).order(:updated_at)
-    @notifications = current_user.passive_notifications.order(:updated_at)
+    @notifications = if params[:which] == "all"
+        current_user.passive_notifications.order(:updated_at)
+      else
+        current_user.passive_notifications.where(checked: false).order(:updated_at)
+      end
   end
 
   def update
