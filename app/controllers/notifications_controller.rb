0d661a0bc855ba87ec8bd8692e87a5a88b9cadfe
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-18 21:25:26 +0900

    通知一覧表示を実装

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,6 @@
 class NotificationsController < ApplicationController
   def index
+    @active_notifications = current_user.active_notifications.where(checked: false).order(:updated_at)
+    @notifications = current_user.active_notifications.order(:updated_at)
   end
 end
