b50680eea47a37afcc000c0933a2a8bbae966ccb
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-18 23:49:15 +0900

    通知既読機能を実装

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,4 +3,10 @@ class NotificationsController < ApplicationController
     @active_notifications = current_user.passive_notifications.where(checked: false).order(:updated_at)
     @notifications = current_user.passive_notifications.order(:updated_at)
   end
+
+  def update
+    @notification = Notification.find(params[:id])
+    @notification.update(checked: true)
+    redirect_to request.referer
+  end
 end
