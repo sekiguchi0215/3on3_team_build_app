fd223008e8cbae7e2ec69fb26577709ec4482dda
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-19 22:55:03 +0900

    チームに招待する通知を作るメソッドを記述

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,16 @@ class Team < ApplicationRecord
   has_many :users, through: :members
   has_many :team_messages, dependent: :destroy
   has_many :notifications, dependent: :destroy
+
+  def team_invitation_notification(current_user, visited_id, team_id)
+    temp = Notification.where(visitor_id: current_user.id, visited_id: visted_id, team_id: team_id)
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        visited_id: visited_id,
+        team_id: team_id,
+        action: "invitation",
+      )
+      notification.save if notification.valid?
+    end
+  end
 end
