5a1cfde743a508dd37420dba1ddb2b5826986c20
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-19 23:43:04 +0900

    不備修正

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,7 +5,7 @@ class Team < ApplicationRecord
   has_many :notifications, dependent: :destroy
 
   def team_invitation_notification(current_user, visited_id, team_id)
-    temp = Notification.where(visitor_id: current_user.id, visited_id: visted_id, team_id: team_id)
+    temp = Notification.where(visitor_id: current_user.id, visited_id: visited_id, team_id: team_id)
     if temp.blank?
       notification = current_user.active_notifications.new(
         visited_id: visited_id,
