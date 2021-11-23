b3c26180457e581f3e5f6a7b064c9224f7fad628
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-23 21:21:36 +0900

    チームを作り、招待を送る際に利用するメソッドを記述

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,7 +4,7 @@ class Team < ApplicationRecord
   has_many :team_messages, dependent: :destroy
   has_many :notifications, dependent: :destroy
 
-  def team_invitation_notification(current_user, visited_id, team_id)
+  def save_team_invitation_notification(current_user, visited_id, team_id)
     temp = Notification.where(visitor_id: current_user.id, visited_id: visited_id, team_id: team_id)
     if temp.blank?
       notification = current_user.active_notifications.new(
@@ -13,6 +13,13 @@ class Team < ApplicationRecord
         action: "invitation",
       )
       notification.save if notification.valid?
+    end
+  end
+
+  def team_invitation_notification(current_user, user_ids, team_id)
+    visited_ids = user_ids.reject(&:empty?)
+    visited_ids.each do |visited_id|
+      save_team_invitation_notification(current_user, visited_id, team_id)
     end
   end
 end
