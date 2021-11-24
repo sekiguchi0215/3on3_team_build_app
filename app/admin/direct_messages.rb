ba1497a47ffdbd09cc8f3512362557ca7a1c69d5
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-25 00:00:55 +0900

    コマンドを実行して、DirctMessage モデルに対応した管理画面を生成

---
+++ app/admin/direct_messages.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register DirectMessage do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :user_id, :room_id, :message, :is_valid
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:user_id, :room_id, :message, :is_valid]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  permit_params :user_id, :room_id, :message, :is_valid
+end
