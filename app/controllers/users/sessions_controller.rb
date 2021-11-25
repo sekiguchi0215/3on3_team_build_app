959c44b071235cdf714d3e9cf68deaa9d59a019e
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-25 23:36:43 +0900

    不備修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,11 @@
 class Users::SessionsController < Devise::SessionsController
+
+  # ゲストログイン
+  def guest_sign_in
+    sign_in User.guest
+    redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
+  end
+
   protected
 
   # ログイン後のリダイレクト先
@@ -9,11 +16,5 @@ class Users::SessionsController < Devise::SessionsController
   # ログアウト後のリダイレクト先
   def after_sign_out_path_for(resource)
     new_user_session_path
-  end
-
-  # ゲストログイン
-  def guest_sign_in
-    sign_in User.guest
-    redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
   end
 end
