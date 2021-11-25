090dad527c51a38376862bf951ac66d19acdbf50
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-25 23:46:00 +0900

    ゲストユーザーを削除できないように設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :ensure_normal_user, only: :destroy
+
+  def ensure_normal_user
+    if resource.email == "guest@example.com"
+      redirect_to root_path, alert: "ゲストユーザーは削除できません。"
+    end
+  end
+
   protected
 
   # 新規登録後のリダイレクト先
