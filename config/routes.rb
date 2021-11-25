d710d0e8dc885a94b8968da3189089e5959268d4
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-25 22:19:52 +0900

    ゲストログイン用のルーティングを記述

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
                        registrations: "users/registrations",
                        sessions: "users/sessions",
                      }
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   resources :users, only: [:index, :show]
   resources :deck_lists
   resources :recruitments do
