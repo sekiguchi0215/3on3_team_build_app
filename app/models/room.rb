8c46dc68f5f241a7aeb8353a440a316f00328a41
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-26 21:26:32 +0900

    ルーム一覧表示ページの表示を修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,12 @@
 class Room < ApplicationRecord
   has_many :room_keys, dependent: :destroy
   has_many :direct_messages, dependent: :destroy
+
+  def show_last_direct_message
+    if (last_message = direct_messages.last).present?
+      last_message.message
+    else
+      "まだメッセージはありません。"
+    end
+  end
 end
