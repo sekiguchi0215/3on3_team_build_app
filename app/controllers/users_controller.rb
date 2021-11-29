aad996bb548dafd5d416af7368c65d308859492b
sekiguchi0215 <dsphere5296@gmail.com>
2021-11-30 00:43:45 +0900

    ユーザー詳細ページの表示・スタイルを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
       @decks = @user.deck_lists.includes(:user).where(status: "public")
     end
     @entries = @user.entried_recruitment
+    @teams = current_user.teams.order(:updated_at)
 
     # ダイレクトメッセージ機能の処理
     @currentUserRoomKey = RoomKey.where(user_id: current_user.id)
@@ -32,5 +33,7 @@ class UsersController < ApplicationController
         @room_key = RoomKey.new
       end
     end
+
+    @rooms = current_user.rooms.order(:updated_at)
   end
 end
