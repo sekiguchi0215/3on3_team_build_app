bf6339cb107a036eaf359dc5e452c6f6716dfd11
sekiguchi0215 <dsphere5296@gmail.com>
2021-12-23 00:40:18 +0900

    TeamMessage モデルの、FactoryBot を記述

--- spec/factories/team_messages.rb
+++ spec/factories/team_messages.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :team_message do
+    user_id { FactoryBot.create(:user).id }
+    team_id { FactoryBot.create(:team).id }
+    message { Faker::Lorem.sentence }
   end
 end
