aeaf6ac694bfd0149ba1bb126c0607a9345dc624
sekiguchi0215 <dsphere5296@gmail.com>
2021-12-23 00:33:21 +0900

    DirectMessage モデルの、FactoryBot を記述

--- spec/factories/direct_messages.rb
+++ spec/factories/direct_messages.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :direct_message do
+    user_id { FactoryBot.create(:user).id }
+    room_id { FactoryBot.create(:room).id }
+    message { Faker::Lorem.sentence }
   end
 end
