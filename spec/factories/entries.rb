8567b3b8416c9961463b74f8b9d302524a4bdd01
sekiguchi0215 <dsphere5296@gmail.com>
2021-12-30 23:59:36 +0900

    記述を追加

--- spec/factories/entries.rb
+++ spec/factories/entries.rb
@@ -3,4 +3,8 @@ FactoryBot.define do
     user_id { FactoryBot.create(:user).id }
     recruitment_id { FactoryBot.create(:recruitment).id }
   end
+
+  trait :skip_validate do
+    to_create { |i| i.save(validate: false) }
+  end
 end
