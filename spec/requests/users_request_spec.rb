1a47358f46c1b1ac3cd6afe808208b7121b3c4ec
sekiguchi0215 <dsphere5296@gmail.com>
2022-01-04 00:02:57 +0900

    User モデルの new アクションの RSpec を記述

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "Users", type: :request do
-  let(:user) { create(:user) }
-  before do
-    sign_in user
-  end
-
   describe "GET #show" do
+    let(:user) { create(:user) }
+    before do
+      sign_in user
+    end
+
     subject { get(user_path(user.id)) }
     it "リクエストが成功する" do
       subject
@@ -23,6 +23,14 @@ RSpec.describe "Users", type: :request do
     it "introduction が表示されている" do
       subject
       expect(response.body).to include user.introduction
+    end
+  end
+
+  describe "GET #new" do
+    subject { get(new_user_session_path) }
+    it "リクエストが成功する" do
+      subject
+      expect(response).to have_http_status(:ok)
     end
   end
 end
