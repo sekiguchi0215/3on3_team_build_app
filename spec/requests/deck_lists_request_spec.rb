6970950eff6fe9b9fbe9a4fb2c8cb65908d73afc
sekiguchi0215 <dsphere5296@gmail.com>
2022-01-05 00:50:28 +0900

    DeckList モデルの index アクションの RSpec を記述

---
+++ spec/requests/deck_lists_request_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe "DeckLists", type: :request do
+  let(:user) { create(:user) }
+  before do
+    sign_in user
+  end
+
+  describe "GET #index" do
+    subject { get(deck_lists_path) }
+
+    context "デッキリストが存在するとき" do
+      let(:deck_list) { build(:deck_list, user_id: user.id) }
+
+      it "リクエストが成功する" do
+        subject
+        expect(response).to have_http_status(200)
+      end
+
+      it "deck_list が表示されている" do
+        subject
+        expect(response.body).to include deck_list.deck_list.to_s
+      end
+    end
+  end
+end
