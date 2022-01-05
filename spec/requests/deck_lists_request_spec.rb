214a0368579cc2b73e97ff7a384675f4c26adbda
sekiguchi0215 <dsphere5296@gmail.com>
2022-01-05 21:42:41 +0900

    DeckList モデルの edit アクションの RSpec を記述

--- spec/requests/deck_lists_request_spec.rb
+++ spec/requests/deck_lists_request_spec.rb
@@ -90,4 +90,23 @@ RSpec.describe "DeckLists", type: :request do
       end
     end
   end
+
+  describe "GET #edit" do
+    subject { get(edit_deck_list_path(deck_list_id)) }
+
+    context "デッキリストが存在するとき" do
+      let(:deck_list) { create(:deck_list, user_id: user.id) }
+      let(:deck_list_id) { deck_list.id }
+
+      it "リクエストが成功する" do
+        subject
+        expect(response).to have_http_status(200)
+      end
+
+      it "deck_list が表示されている" do
+        subject
+        expect(response.body).to include deck_list.deck_list.url
+      end
+    end
+  end
 end
